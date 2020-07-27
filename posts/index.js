2a3d0971cba412d3136760f498cc86378c4f3977
Andrew <andrew@masterdef.net>
2020-07-27 13:38:42 +0300

    Blog comments service

--- posts/index.js
+++ posts/index.js
@@ -14,8 +14,10 @@
  app.post('/posts', (req, res) => {
    const id = randomBytes(4).toString('hex');
 
-   const {title} = req.body;
-   posts[id] = {id, title};
+   const { title }  = req.body;
+   posts[id]        = {id, title};
+
+   res.status(201).send(posts[id]);
  });
 
  app.listen(4000, () => {
