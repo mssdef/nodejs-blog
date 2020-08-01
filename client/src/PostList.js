f769ed13fc007c643a4669e56c7871e38376b210
Andrew <andrew@masterdef.net>
2020-08-01 17:34:09 +0300

    create posts UI

---
+++ client/src/PostList.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default () => {
+  const [ posts, setPosts ] = useState({});
+
+  const fetchPosts = async () => {
+    const res = await axios.get('http://localhost:4000/posts');
+
+    setPosts(res.data);
+  }
+
+  useEffect(()=> {
+    fetchPosts();
+  }, []);
+
+  const renderedPosts = Object.values(posts).map(post=> {
+     return <div className="card" key={post.id}>
+      <h3>{post.title}</h3>
+     </div>
+  });
+
+  return <div className="posts-list d-flex flex-row flex-wrap justify-content-between">{renderedPosts}</div>;
+}
