ea257b2c31ab110b2634f974060c8e05754eaaf2
Andrew <andrew@masterdef.net>
2020-08-14 15:12:24 +0300

    events bus

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@
  const bodyParser = require('body-parser');
  const { randomBytes } = require('crypto');
  const cors = require('cors');
+ const axios = require('axios');
 
  const app = express();
  app.use(bodyParser.json());
@@ -14,7 +15,7 @@
    res.send(commentsByPostId[req.params.id] || []);
  });
 
- app.post('/posts/:id/comments', (req, res) => {
+ app.post('/posts/:id/comments', async(req, res) => {
    const id = randomBytes(4).toString('hex');
 
    const { content }  = req.body;
@@ -24,6 +25,15 @@
 
    comments.push({id, content});
    commentsByPostId[req.params.id] = comments;
+
+   // submit event
+   await axios.post('http://localhost:4005/events', {
+     type: 'CommentCreated',
+     data: {
+       id, content,
+       postId: req.params.id
+     }
+   });
 
    res.status(201).send(comments);
  });
